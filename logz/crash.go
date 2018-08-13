892d9f2f78705f50e7459de220aadc5ea0f58717
Anund McKague <amckague@atlassian.com>
2018-08-13 10:22:08 +1000

    Tweak parameters slightly, document zap failing

--- logz/crash.go
+++ logz/crash.go
@@ -11,12 +11,15 @@ import (
 
 func LogStructuredPanic() {
 	if r := recover(); r != nil {
-		logStructuredPanic(os.Stderr, r, time.Now().Format(time.RFC3339), debug.Stack())
+		logStructuredPanic(os.Stderr, r, time.Now(), debug.Stack())
 		panic(r)
 	}
 }
 
-func logStructuredPanic(out io.Writer, i interface{}, time string, stack []byte) {
+// internally zap is overly opinionated about what to do when the log level is fatal or panic
+// it chooses to call os.exit or panic if the level is set. There does not appear to be a simple
+// way to work around that choice so we build the log message by hand instead.
+func logStructuredPanic(out io.Writer, panicValue interface{}, now time.Time, stack []byte) {
 	bytes, err := json.Marshal(struct {
 		Level   string `json:"level"`
 		Time    string `json:"time"`
@@ -24,13 +27,13 @@ func logStructuredPanic(out io.Writer, i interface{}, time string, stack []byte)
 		Stack   string `json:"stack"`
 	}{
 		Level:   "fatal",
-		Time:    time,
-		Message: fmt.Sprintf("%v", i),
+		Time:    now.Format(time.RFC3339),
+		Message: fmt.Sprintf("%v", panicValue),
 		Stack:   string(stack),
 	})
 	if err != nil {
 		fmt.Fprintf(out, "error while serializing panic: %+v\n", err) // nolint: errcheck, gas
-		fmt.Fprintf(out, "original panic: %+v\n", i)                  // nolint: errcheck, gas
+		fmt.Fprintf(out, "original panic: %+v\n", panicValue)         // nolint: errcheck, gas
 		return
 	}
 	fmt.Fprintf(out, "%s\n", bytes) // nolint: errcheck, gas
