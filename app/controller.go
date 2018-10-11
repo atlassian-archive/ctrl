5ad87b24e042f7a9cb7ac2f6874a4a018a0e86b0
Mikhail Mazurskiy <mmazurskiy@atlassian.com>
2018-10-11 17:15:24 +1100

    Namespaced controller options

--- app/controller.go
+++ app/controller.go
@@ -13,12 +13,20 @@ const (
 
 type GenericControllerOptions struct {
 	ResyncPeriod time.Duration
-	Namespace    string
 	Workers      int
 }
 
 func BindGenericControllerFlags(o *GenericControllerOptions, fs *flag.FlagSet) {
 	fs.DurationVar(&o.ResyncPeriod, "resync-period", defaultResyncPeriod, "Resync period for informers")
 	fs.IntVar(&o.Workers, "workers", 2, "Number of workers that handle events from informers")
+}
+
+type GenericNamespacedControllerOptions struct {
+	GenericControllerOptions
+	Namespace string
+}
+
+func BindGenericNamespacedControllerFlags(o *GenericNamespacedControllerOptions, fs *flag.FlagSet) {
+	BindGenericControllerFlags(&o.GenericControllerOptions, fs)
 	fs.StringVar(&o.Namespace, "namespace", meta_v1.NamespaceAll, "Namespace to use. All namespaces are used if empty string or omitted")
 }
