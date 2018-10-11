7904b9025cbba6d428cdd458aed84cb35aa7a39b
Mikhail Mazurskiy <mmazurskiy@atlassian.com>
2018-10-11 15:36:03 +1100

    Remove 'from environment' client configuration support

--- client/config.go
+++ client/config.go
@@ -1,33 +1,11 @@
 package client
 
 import (
-	"net"
-	"os"
-
 	"github.com/pkg/errors"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
-
-func ConfigFromEnv() (*rest.Config, error) {
-	host, port := os.Getenv("KUBERNETES_SERVICE_HOST"), os.Getenv("KUBERNETES_SERVICE_PORT")
-	if host == "" || port == "" {
-		return nil, errors.New("unable to load cluster configuration, KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT must be defined")
-	}
-	CAFile, CertFile, KeyFile := os.Getenv("KUBERNETES_CA_PATH"), os.Getenv("KUBERNETES_CLIENT_CERT"), os.Getenv("KUBERNETES_CLIENT_KEY")
-	if CAFile == "" || CertFile == "" || KeyFile == "" {
-		return nil, errors.New("unable to load TLS configuration, KUBERNETES_CA_PATH, KUBERNETES_CLIENT_CERT and KUBERNETES_CLIENT_KEY must be defined")
-	}
-	return &rest.Config{
-		Host: "https://" + net.JoinHostPort(host, port),
-		TLSClientConfig: rest.TLSClientConfig{
-			CAFile:   CAFile,
-			CertFile: CertFile,
-			KeyFile:  KeyFile,
-		},
-	}, nil
-}
 
 func LoadConfig(configFileFrom, configFileName, configContext string) (*rest.Config, error) {
 	var config *rest.Config
@@ -36,8 +14,6 @@ func LoadConfig(configFileFrom, configFileName, configContext string) (*rest.Con
 	switch configFileFrom {
 	case "in-cluster":
 		config, err = rest.InClusterConfig()
-	case "environment":
-		config, err = ConfigFromEnv()
 	case "file":
 		var configAPI *clientcmdapi.Config
 		configAPI, err = clientcmd.LoadFromFile(configFileName)
